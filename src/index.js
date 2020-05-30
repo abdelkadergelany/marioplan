f46eb1f39004a1b5d399f2880ba9712c455f7212
aly-abdelkader-iut-cse-4301 <38514887+aly-abdelkader-iut-cse-4301@users.noreply.github.com>
2020-05-31 00:20:43 +0600

    lesson 26-30

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux'
+import {Provider,useSelector} from 'react-redux'
 import thunk from 'redux-thunk'
 import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import { ReactReduxFirebaseProvider, getFirebase,isLoaded } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'
 import firebase from 'firebase/app'
 
@@ -27,13 +27,14 @@ const rrfProps = {
   createFirestoreInstance
 };
 
-ReactDOM.render(
-  <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-          <App />
-      </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById("root")
-);
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+      return children
+}
+
+ReactDOM.render(<Provider store={store}><ReactReduxFirebaseProvider {...rrfProps}>
+  <AuthIsLoaded><App /> </AuthIsLoaded></ReactReduxFirebaseProvider>
+</Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
